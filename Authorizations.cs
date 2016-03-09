42bac73e1c5bc65ce8bd73669b56c30b0b6f7f53
joshcodes <josh@blackbarlabs.com>
2016-03-09 13:00:32 -0600

    initial

---
+++ Authorizations.cs
@@ -0,0 +1,64 @@
+﻿using BlackBarLabs.Security.Authorization;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+
+namespace BlackBarLabs.Security.AuthorizationClient
+{
+    public static class Authorizations
+    {
+        private class Authorization : IAuthorization
+        {
+            public CredentialsType[] CredentialProviders { get; set; }
+
+            public Guid Id { get; set; }
+        }
+
+        public static void CreateImplicitVoucherAsync(Guid authId, string password)
+        {
+            var authServerLocation = ConfigurationManager.AppSettings["BlackBarLabs.Security.AuthorizationClient.ServerUrl"];
+            
+            var trustedVoucherProverId = CredentialProvider.Voucher.Utilities.GetTrustedProviderId();
+            var token = CredentialProvider.Voucher.Utilities.GenerateToken(authId, DateTime.UtcNow + TimeSpan.FromMinutes(10.0));
+
+            var credentialVoucher = new CredentialsType
+            {
+                Method = CredentialValidationMethodTypes.Voucher,
+                Provider = trustedVoucherProverId,
+                Token = token,
+                UserId = authId.ToString("N"),
+            };
+            var credentialImplicit = new CredentialsType
+            {
+                Method = CredentialValidationMethodTypes.Implicit,
+                Provider = new Uri("http://www.example.com/Auth"),
+                Token = "Password#1",
+                UserId = authId.ToString("N"),
+            };
+
+            var auth = new Authorization()
+            {
+                Id = Guid.NewGuid(),
+                CredentialProviders = new CredentialsType []
+                {
+                    credentialVoucher,
+                    credentialImplicit,
+                }
+            };
+
+            var authJson = Newtonsoft.Json.JsonConvert.SerializeObject(auth);
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(authServerLocation + "/api/Authorization");
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(authJson);
+                streamWriter.Flush();
+            }
+            var createAuthResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+        }
+    }
+}
