022cbc5431841ccf129b8696e2a3aed7bacae7c5
joshcodes <josh@blackbarlabs.com>
2016-03-21 21:06:47 -0500

    added invertable service

---
+++ IContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlackBarLabs.Security.Authorization;
+using System.Net;
+
+namespace BlackBarLabs.Security.AuthorizationClient
+{
+    public interface IContext
+    {
+        Task<TResult> ClaimsGetAsync<TResult>(Uri claimsLocation, Func<IClaim[], TResult> success, Func<HttpStatusCode, string, TResult> error);
+    }
+}
