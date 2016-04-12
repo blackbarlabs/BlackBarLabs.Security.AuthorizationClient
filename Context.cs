bdff247970f89eec9452994067810e0dbc379eda
Jeremy Randolph <randocodes@gmail.com>
2016-04-11 22:03:21 -0500

    Implementing Interfaces

--- Context.cs
+++ Context.cs
@@ -34,6 +34,31 @@ namespace BlackBarLabs.Security.AuthorizationClient
                 failure);
         }
 
+        public Task<TResult> CreateAuthorizationAsync<TResult>(Guid accountId, Func<TResult> onSuccess, Func<string, TResult> onFailure)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> CreateSessionsWithImplicitAsync<TResult>(string username, string password, Func<string, string, TResult> success, Func<string, TResult> failed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> CreateCredentialVoucherAsync<TResult>(Guid accountId, TimeSpan timeSpan, Func<string, TResult> success, Func<string, TResult> failure)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> CreateCredentialImplicitAsync<TResult>(Guid accountId, string username, string password, Func<TResult> success, Func<string, TResult> failure)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> AuthorizationDeleteAsync<TResult>(Guid id, Func<TResult> success, Func<HttpStatusCode, string, TResult> webFailure, Func<TResult> failure)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<TResult> ClaimPutAsync<TResult>(Guid accountId, Uri distributorAdminRole, string value, Func<TResult> success, Func<TResult> notFound, Func<HttpStatusCode, string, TResult> httpError, Func<string, TResult> failure)
         {
             throw new NotImplementedException();
